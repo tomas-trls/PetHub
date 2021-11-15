09b3e6f34da716f0c22987826039226dacdb7e77
muinat jimoh <ojimoh21@live.co.uk>
2021-11-15 16:11:59 +0000

    devise user model created

---
+++ db/migrate/20211115155029_add_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :text
+    add_column :users, :postcode, :string
+    add_column :users, :owner, :boolean
+  end
+end
