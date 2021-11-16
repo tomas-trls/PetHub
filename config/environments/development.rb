87319bc5d4924de35a9b0bd96b1a807e938e3fe7
muinat jimoh <ojimoh21@live.co.uk>
2021-11-16 11:47:48 +0000

    cloudinary added to project

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.active_storage.service = :cloudinary
 end
