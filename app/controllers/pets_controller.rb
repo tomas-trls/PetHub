dd86a1390042b02695250bc2c4fb8b1abda80215
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-11-17 13:31:54 +0000

    pet new done, cl_img added to pet index and show, authentication corrected

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,9 +7,27 @@ class PetsController < ApplicationController
     @pets = Pet.all
   end
 
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
+  end
+
   private
 
   def find_pet
     @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :description, :breed, :age, :photo)
   end
 end
