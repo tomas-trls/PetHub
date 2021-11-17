77bdfcb8f05615ce00b8b969ead96ecdb0eb4968
Gulce Sakallioglu <gulcesakallioglu@gmail.com>
2021-11-17 13:39:44 +0000

    map and markers done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,7 +8,9 @@ class PetsController < ApplicationController
     @markers = @pets.geocoded.map do |pet|
       {
         lat: pet.latitude,
-        lng: pet.longitude
+        lng: pet.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { pet: pet }),
+        image_url: helpers.asset_url("redpaw.png")
       }
     end
   end
--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -12,7 +12,32 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb'
     });
+
+    this._addMarkersToMap();
+    this._fitMapToMarkers();
+  }
+
+  _addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+      const customMarker = document.createElement('div');
+      customMarker.className = 'marker';
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`;
+      customMarker.style.backgroundSize = 'contain';
+      customMarker.style.width = '25px';
+      customMarker.style.height = '25px';
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(this.map);
+    });
+  }
+
+  _fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+    this.map.fitBounds(bounds, { padding: 30, maxZoom: 15, duration: 0 });
   }
 }
