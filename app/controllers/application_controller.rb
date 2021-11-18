8da4c9737037bcafeadbcbf9f22b2d8076b021ae
muinat jimoh <ojimoh21@live.co.uk>
2021-11-18 13:32:21 +0000

    User profile has been added and default if no picture added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :postcode, :owner])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :postcode, :owner, :photo])
   end
 
   def home; end
