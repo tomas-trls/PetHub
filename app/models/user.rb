0de30ec63269e3bb9f69cb1ba0941f1ec1da4860
Gulce Sakallioglu <gulcesakallioglu@gmail.com>
2021-11-18 11:43:07 +0000

    added delete pet function and associated validations to pet, user and booking models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
   has_many :pets
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
